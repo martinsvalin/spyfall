63d411a981a3c2d68e9184db1af932dbc91b84ac
Jeffrey Örnerstedt <jeffrey.ornerstedt@m.co>
2020-04-17 11:08:27 +0200

    wip - 1

--- lib/spyfall_web/controllers/page_controller.ex
+++ lib/spyfall_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule SpyfallWeb.PageController do
   use SpyfallWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    assign(conn, :locations, ["1", "2", "3"])
+    |> render("index.html")
   end
 end
