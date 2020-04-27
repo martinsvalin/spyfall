8cd514fb321c505330f83e4b333a8b6cd91e09a1
Martin Svalin <martin@lite.nu>
2020-04-27 19:56:18 +0200

    Track and show updates to players

--- lib/spyfall_web/live/game_live.ex
+++ lib/spyfall_web/live/game_live.ex
@@ -9,17 +9,17 @@ defmodule SpyfallWeb.GameLive do
     PubSub.subscribe(Spyfall.PubSub, topic)
     Players.track(self(), topic, socket.id, %{name: initial_name()})
 
-    players = Players.list(topic) |> Enum.map(&dig_out_name/1)
+    new_socket =
+      socket
+      |> assign(:topic, topic)
+      |> assign(:players, players(topic))
 
-    {:ok, assign(socket, :players, players)}
+    {:ok, new_socket}
   end
 
   @impl true
-  def handle_info(
-        %{event: "presence_diff", payload: %{joins: joins, leaves: leaves}},
-        %{assigns: %{players: players}} = socket
-      ) do
-    {:noreply, socket}
+  def handle_info(%{event: "presence_diff"}, socket) do
+    {:noreply, socket |> assign(:players, players(socket.assigns.topic))}
   end
 
   @impl true
@@ -33,7 +33,12 @@ defmodule SpyfallWeb.GameLive do
     "wonder woman"
   end
 
-  def dig_out_name({_presence_id, %{metas: metas}}) do
+  defp players(topic) do
+    Players.list(topic)
+    |> Enum.map(&dig_out_name/1)
+  end
+
+  defp dig_out_name({_presence_id, %{metas: metas}}) do
     Enum.find_value(metas, fn map -> map[:name] end)
   end
 end
