09d9b6ae08f997f24b0603b3f77d1e0c903ed26b
Martin Svalin <martin@lite.nu>
2020-04-28 21:25:46 +0200

    Only register and track when liveview is connected. Move template to own file

--- lib/spyfall_web/live/game_live.ex
+++ lib/spyfall_web/live/game_live.ex
@@ -5,32 +5,25 @@ defmodule SpyfallWeb.GameLive do
 
   @impl true
   def mount(%{"game_id" => game_id}, _session, socket) do
-    topic = "game:#{game_id}"
-    PubSub.subscribe(Spyfall.PubSub, topic)
-    Player.track(self(), topic, socket.id, %{})
+    if connected?(socket) do
+      Games.register(game_id, socket.id)
 
-    Games.register(game_id, socket.id)
+      topic = "game:#{game_id}"
+      PubSub.subscribe(Spyfall.PubSub, topic)
+      Player.track(self(), topic, socket.id, %{})
+    end
 
-    new_socket =
-      socket
-      |> assign(:game, game_id)
-      |> assign(:online, Player.online(game_id))
-
-    {:ok, new_socket}
+    {:ok, assign(socket, game: game_id, player_id: socket.id, online: online_players(game_id))}
   end
 
   @impl true
   def handle_info(%{event: "presence_diff"}, socket) do
-    {:noreply, assign(socket, :online, Player.online(socket.assigns.game))}
+    {:noreply, assign(socket, :online, online_players(socket.assigns.game))}
   end
 
-  @impl true
-  def render(assigns) do
-    ~L"""
-    <h2>Players online</h2>
-    <ul>
-      <%= for {_id, player} <- Map.take(Games.players(assigns.game), assigns.online), do: raw("<li>#{player.name}</li>") %>
-    </ul>
-    """
+  defp online_players(game_id) do
+    Games.players(game_id)
+    |> Map.take(Player.online(game_id))
+    |> Map.values()
   end
 end
