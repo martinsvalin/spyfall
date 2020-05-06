9ce86a040c6db16835ab35b8a59eaa6afabba046
Martin Svalin <martin@lite.nu>
2020-05-06 22:10:49 +0200

    Ready to deploy to heroku

--- lib/spyfall_web/endpoint.ex
+++ lib/spyfall_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule SpyfallWeb.Endpoint do
   ]
 
   socket "/socket", SpyfallWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
