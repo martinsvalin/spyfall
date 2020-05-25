e4ac40eacb74e8bb15b4113c3719faabf4437d80
Martin Svalin <martin@lite.nu>
2020-05-26 00:12:16 +0200

    Use tailwind css

---
+++ assets/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    purge: [
+        "../**/*.html",
+        "../**/*.html.eex",
+        "../**/*.html.leex",
+        "../**/views/**/*.ex",
+        "../**/live/**/*.ex",
+        "./js/**/*.js"
+    ],
+    theme: {},
+    variants: {},
+    plugins: []
+};
