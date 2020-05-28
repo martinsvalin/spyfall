4ca0ae95bebaf1aa7ec19cbf95a6f112f4e48b84
Martin Svalin <martin@lite.nu>
2020-05-28 14:30:10 +0200

    Fix mysterious issue with npm run deploy

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,13 +1,12 @@
 module.exports = {
-    purge: [
-        "../**/*.html",
-        "../**/*.html.eex",
-        "../**/*.html.leex",
-        "../**/views/**/*.ex",
-        "../**/live/**/*.ex",
-        "./js/**/*.js"
-    ],
-    theme: {},
-    variants: {},
-    plugins: []
-};
+  purge: [
+    "../**/*.html.eex",
+    "../**/*.html.leex",
+    "../**/views/**/*.ex",
+    "../**/live/**/*.ex",
+    "./js/**/*.js",
+  ],
+  theme: {},
+  variants: {},
+  plugins: [],
+}
